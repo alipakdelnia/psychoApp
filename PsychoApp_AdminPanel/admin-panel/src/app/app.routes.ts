e8c656890bed6adcc3c6ccf59f7ec1cb898caff6
Ali Pakdelnia <porfosorap@gmail.com>
2024-12-23 00:08:47 +0330

    delete old admin panel and change ip

--- PsychoApp_AdminPanel/admin-panel/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-
-export const routes: Routes = [
-   
-  ];
--- PsychoApp_BackEnd/obj/Debug/net9.0/psychoApp.AssemblyInfo.cs
+++ PsychoApp_BackEnd/obj/Debug/net9.0/psychoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71e5506d5f3364d9b8513c71ffb2bb673cdcfc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf73a225e2010097cc3cdf750297101bf110702")]
 [assembly: System.Reflection.AssemblyProductAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
