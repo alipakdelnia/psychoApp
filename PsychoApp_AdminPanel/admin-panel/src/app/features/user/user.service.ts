5b5059989620cb8537243811af4c80fa01d656cb
Ali Pakdelnia <porfosorap@gmail.com>
2024-12-23 00:29:17 +0330

    add user table in admin panel

---
+++ PsychoApp_AdminPanel/admin-panel/src/app/features/user/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost:5087/api/users'; // URL سرور
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
--- PsychoApp_BackEnd/obj/Debug/net9.0/psychoApp.AssemblyInfo.cs
+++ PsychoApp_BackEnd/obj/Debug/net9.0/psychoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf73a225e2010097cc3cdf750297101bf110702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8c656890bed6adcc3c6ccf59f7ec1cb898caff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("psychoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
