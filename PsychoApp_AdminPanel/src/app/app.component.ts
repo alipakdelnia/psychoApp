a219d9d4aa9dc7c9215db081c7407f1a8a04e778
Ali Pakdelnia <porfosorap@gmail.com>
2024-12-24 20:51:10 +0330

    add translate package for two lang in admin panel

--- PsychoApp_AdminPanel/src/app/app.component.ts
+++ PsychoApp_AdminPanel/src/app/app.component.ts
@@ -3,23 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { UsersComponent } from "./features/users/users.component";
 import { LanguageService } from './language-service.service';
 import { NgClass } from '@angular/common';
+import { TranslateDirective, TranslatePipe ,TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent,NgClass],
+  imports: [RouterOutlet, UsersComponent,NgClass,TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent{
   title = 'PsychoApp_AdminPanel';
 
-  constructor(private languageService: LanguageService) {}
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+}
 
-
-
-  setLanguage(lang: 'en' | 'fa'): void {
-    this.languageService.setLanguage(lang);
-  }
+switchLanguage(lang: string): void {
+  this.translate.use(lang);
+  document.documentElement.lang = lang;
+  document.documentElement.dir = lang === 'fa' ? 'rtl' : 'ltr'; // change html dir
+}
   
 }
--- PsychoApp_AdminPanel/src/app/app.config.ts
+++ PsychoApp_AdminPanel/src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { mainInterceptorInterceptor } from './maininterceptor.interceptor';
+import { provideTranslateService, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+const httpLoaderFactory: (http: HttpClient) => TranslateHttpLoader = (http: HttpClient) =>
+  new TranslateHttpLoader(http, 'assets/i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,7 +19,14 @@ export const appConfig: ApplicationConfig = {
       multi: true
     },
     provideZoneChangeDetection({ eventCoalescing: true }),
-     provideRouter(routes),
-      provideClientHydration(), 
-      provideHttpClient(withFetch())]
+    provideHttpClient(withFetch()),
+    provideTranslateService({ defaultLanguage: 'en',loader: {
+      provide: TranslateLoader,
+      useFactory: httpLoaderFactory,
+      deps: [HttpClient],
+    }}),
+    provideRouter(routes),
+    provideClientHydration()
+  ]
 };
+
